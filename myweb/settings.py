b523f7d12a773f32e6705f6e1dbc79024cbb5d5b
serina <bakunziserina@gmail.com>
2019-11-20 10:56:58 +0300

    My first blog in django, editted settings

--- myweb/settings.py
+++ myweb/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'r%t6)l-npj2ov1qot5)nf0o^oj2$f8_7+**yyd+qr0=tvb+c_y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ["code.pythonanywhere.com"]
 
 
 # Application definition
